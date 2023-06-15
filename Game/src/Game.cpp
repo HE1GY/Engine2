a1000ee86adf173fcaad9428c13e62f0ea8d9860
Helgy <oleglavro6@gmail.com>
2023-06-15 17:39:34 +0300

    setup pre-commit check mandatory for all

--- Game/src/Game.cpp
+++ Game/src/Game.cpp
@@ -1,34 +1,26 @@
+#include "GameLayer.h"
 #include <Engine/Engine.h>
 #include <Engine/EntryPoint.h>
-#include "GameLayer.h"
 
-namespace Game
-{
-/**
- * Main custom application
- */
-	class Game : public Engine::Application
-	{
-	public:
-		Game()
-		  : Engine::Application({ 600, 400, "Game" })
-		{
-			INFO_LOG("Start game");
-			PushLayer(Engine::CreateRef<GameLayer>("GameLayer"));
-		}
+namespace Game {
+    /**
+     * Main custom application
+     */
+    class Game : public Engine::Application {
+    public:
+        Game() : Engine::Application({600, 400, "Game"}) {
+            INFO_LOG("Start game");
+            PushLayer(Engine::CreateRef<GameLayer>("GameLayer"));
+        }
 
-		~Game() override = default;
-	};
-}
+        ~Game() override = default;
+    };
+} // namespace Game
 
 /**
  * Method substitutes custom app fo Engine framework
  * @return custom app
  */
-Engine::Application* CreateApplication()
-{
-	return new Game::Game();
+Engine::Application* CreateApplication() {
+    return new Game::Game();
 }
-
-
-
