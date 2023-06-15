75c8a59f36e3ae477f2e7ec60a10d2541704e9d0
Helgy <oleglavro6@gmail.com>
2023-06-15 14:49:43 +0300

    feat: setup LTO

---
+++ cmake/LTO.cmake
@@ -0,0 +1,11 @@
+function(target_enable_lto TARGET)
+    include(CheckIPOSupported)
+    check_ipo_supported(RESULT result OUTPUT output)
+
+    if (result)
+        message(STATUS "IPO/LTO is supported!")
+        set_property(TARGET ${TARGET} PROPERTY INTERPROCEDURAL_OPTIMIZATION ${ENABLE})
+    else ()
+        message(WARNING "IPO/LTO is not supported!")
+    endif ()
+endfunction(target_enable_lto)
