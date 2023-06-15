9a3104ffd08af446b003e86df462cdece953f3a0
Helgy <oleglavro6@gmail.com>
2023-06-15 20:59:56 +0300

    chore: reformat files

--- Engine/src/Engine/LowAPI/Input/Input.h
+++ Engine/src/Engine/LowAPI/Input/Input.h
@@ -8,9 +8,7 @@ namespace Engine {
      */
     class Input {
     public:
-        static void Init(Ref<Window>& window) {
-            m_window = window;
-        }
+        static void Init(Ref<Window>& window) { m_window = window; }
         static bool IsMouseButtonPress(MouseButtonCode button);
         static bool IsKeyPress(KeyCode key_code);
         static std::pair<uint32_t, uint32_t> GetMousePos();
--- Engine/src/Engine/LowAPI/Renderer/VertexArray.cpp
+++ Engine/src/Engine/LowAPI/Renderer/VertexArray.cpp
@@ -3,7 +3,5 @@
 #include "Platform/OpenGL/OpenGLVertexArray.h"
 namespace Engine {
     // TODO switch to platform selector
-    Ref<VertexArray> VertexArray::Create() {
-        return std::make_shared<OpenGLVertexArray>();
-    }
+    Ref<VertexArray> VertexArray::Create() { return std::make_shared<OpenGLVertexArray>(); }
 } // namespace Engine
